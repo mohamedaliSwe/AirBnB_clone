72a8fc347dabeb4fc340432a740c06db04a54032
mohamedaliSwe <mohamedcali350@gmail.com>
2025-09-14 23:59:21 +0300

    Initialized the console

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Command interpreter for the AirBnB clone Project
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand is the command-line interface (CLI) for the AirBnB clone.
+    It allows users to create and manage instances of different models.
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the commandline using Ctrl + D"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on emptyline input"""
+        pass
+
+    def do_help(self, arg):
+        """List available commands using 'help' or
+        command-specific help using 'help <command>'"""
+        return super().do_help(arg)
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
