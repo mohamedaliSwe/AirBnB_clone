c984f47225d7de0b61481a1f4729bf8899a91ce4
mohamedaliSwe <mohamedcali350@gmail.com>
2025-09-15 00:18:32 +0300

    Initialized the console

--- console.py
+++ console.py
@@ -13,21 +13,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Quit command to exit the program\n"""
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """Exit the commandline using Ctrl + D\n"""
+        """EOF signal to exit the program."""
         return True
 
     def emptyline(self):
         """Do nothing on emptyline input"""
         pass
-
-    def do_help(self, arg):
-        """List available commands using 'help' or
-        command-specific help using 'help <command>'"""
-        return super().do_help(arg)
 
 
 if __name__ == "__main__":
