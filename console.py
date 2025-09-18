e339bd46be90c1d8c0fc3d810ca37ca62c21be88
mohamedaliSwe <mohamedcali350@gmail.com>
2025-09-18 10:13:18 +0300

    Correcting errors on console finally

--- console.py
+++ console.py
@@ -1,209 +1,30 @@
 #!/usr/bin/python3
 """
-Entry point to the command interpreter
+Command interpreter for the AirBnB clone Project
 """
 import cmd
-# from models.base_model import BaseModel
-# from models.user import User
-# from models.state import State
-# from models.city import City
-# from models.amenity import Amenity
-# from models.place import Place
-# from models.review import Review
-# from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
     """
-    This Module is the entry point to interpreter for the AIRBNB_CLONE.
-
-    It provides a command-line interface for managing instances of
-    various classes (BaseModel, User, State, City, Amenity, Place,
-    Review) within the storage system.
-
-    Example Usage:
-    $ ./console.py
-    (hbnb)
-
-    (hbnb) help
-
-    Documented commands (type help <topic>):
-    ========================================
-    EOF  all  create  destroy  help  quit  show  update
-
-    (hbnb) help quit
-
-    $
+    HBNBCommand is the command-line interface (CLI) for the AirBnB clone.
+    It allows users to create and manage instances of different models.
     """
     prompt = "(hbnb) "
-    # classes = {
-    #     'BaseModel': BaseModel,
-    #     'User': User,
-    #     'State': State,
-    #     'City': City,
-    #     'Amenity': Amenity,
-    #     'Place': Place,
-    #     'Review': Review
-    # }
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program
-        """
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program on EOF (Ctrl+D)
-        """
+        """EOF signal to exit the program."""
         print("")
         return True
 
-    def emptyline(self):
-        """
-        Do nothing on empty input line
-        """
-        pass
 
-    # def do_create(self, arg):
-    #     """
-    #     Creates a new instance of BaseModel,
-    #     save it and print the id
-    #     """""
-    #     if not arg:
-    #         print("** class name missing **")
-    #         return
-
-    #     if arg not in self.classes:
-    #         print("** class doesn't exist **")
-    #         return
-
-    #     new_instance = self.classes[arg]()
-    #     new_instance.save()
-    #     print(new_instance.id)
-
-    # def do_show(self, arg):
-    #     """
-    #     Prints the string representation of an instance
-    #     based on the class name and id
-    #     """
-    #     args = arg.split()
-
-    #     if not args:
-    #         print("** class name missing **")
-    #         return
-
-    #     if args[0] not in self.classes:
-    #         print("** class doesn't exist **")
-    #         return
-
-    #     if len(args) < 2:
-    #         print("** instance id missing **")
-    #         return
-
-    #     key = "{}.{}".format(args[0], args[1])
-    #     if key not in storage.all():
-    #         print("** no instance found **")
-    #         return
-
-    #     print(storage.all()[key])
-
-    # def do_destroy(self, arg):
-    #     """
-    #     Deletes an instance based on the class name and id
-    #     """
-    #     args = arg.split()
-
-    #     if not args:
-    #         print("** class name missing **")
-    #         return
-
-    #     if args[0] not in self.classes:
-    #         print("** class doesn't exist **")
-    #         return
-
-    #     if len(args) < 2:
-    #         print("** instance id missing **")
-    #         return
-
-    #     key = "{}.{}".format(args[0], args[1])
-    #     if key not in storage.all():
-    #         print("** no instance found **")
-    #         return
-
-    #     del storage.all()[key]
-    #     storage.save()
-
-    # def do_all(self, arg):
-    #     """
-    #     Prints all string representation of all
-    #     instances based or not on the class name.
-    #     """
-    #     if arg and arg not in self.classes:
-    #         print("** class doesn't exist **")
-    #         return
-
-    #     objects = storage.all()
-    #     obj_list = [str(obj) for key, obj in objects.items()
-    #                 if not arg or key.startswith(arg + '.')]
-    #     print(obj_list)
-
-    # def do_update(self, arg):
-    #     """
-    #     Updates an instance based on the class name
-    #     and id by adding or updating attribute
-    #     """
-    #     args = arg.split()
-
-    #     if not args:
-    #         print("** class name missing **")
-    #         return
-
-    #     if args[0] not in self.classes:
-    #         print("** class doesn't exist **")
-    #         return
-
-    #     if len(args) < 2:
-    #         print("** instance id missing **")
-    #         return
-
-    #     key = "{}.{}".format(args[0], args[1])
-    #     if key not in storage.all():
-    #         print("** no instance found **")
-    #         return
-
-    #     if len(args) < 3:
-    #         print("** attribute name missing **")
-    #         return
-
-    #     if len(args) < 4:
-    #         print("** value missing **")
-    #         return
-
-    #     instance = storage.all()[key]
-    #     attr_name = args[2]
-    #     attr_value = args[3]
-
-    #     if hasattr(instance, attr_name):
-    #         attr_type = type(getattr(instance, attr_name))
-    #         try:
-    #             attr_value = attr_type(attr_value)
-    #         except ValueError:
-    #             print("** invalid value **")
-    #             return
-    #     else:
-    #         attr_value = str(attr_value)
-
-    #     setattr(instance, attr_name, attr_value)
-    #     instance.save()
-
-    def do_help(self, arg):
-        """
-        List available commands with 'help' or
-        detailed help with 'help <command>'
-        """
-        return super().do_help(arg)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
